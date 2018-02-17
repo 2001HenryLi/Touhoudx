a5bcf2c71df5b4daba4f7e579db1ff68a5aea8af
TheBestestGuestest <gamegeek99@gmail.com>
2018-02-17 14:38:09 -0800

    yee
    yee2

---
+++ src/com/th/Player.java
@@ -0,0 +1,24 @@
+package com.th;
+
+import java.awt.*;
+
+public class Player {
+    public Image sprite;
+    public Image hitMarker;
+
+    public int x, y;
+    public int vx, vy;
+
+    public Player(){
+        sprite = Toolkit.getDefaultToolkit().getImage("resources\\Character Sprites\\cirno.png");;
+        x = 100;
+        y = 100;
+        vx = 5;
+        vy = 5;
+    }
+    public void move(int xDir, int yDir){
+        x += vx*xDir;
+        y += vy*yDir;
+        System.out.println("moved");
+    }
+}
