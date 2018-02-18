ea30e936edc87c337368da4da644f506733cbf3c
golfconnor <36574331+golfconnor@users.noreply.github.com>
2018-02-17 20:36:18 -0800

    Update Function.java

--- src/com/th/Function.java
+++ src/com/th/Function.java
@@ -15,39 +15,44 @@
 public class Function {
 	
 	private int functiontype;
-	private double constant;
+	private int constant;
 	
-	public Function() {
-	}
-	
-	public Function(int f, double c){
+	public Function(int f, int c){
 		functiontype = f;
 		constant = c;
 	}
 	
+	//testing method
+	public static void main(String [] args){
+		Function f = new Function(4,5);
+		System.out.println(f.getFunction());
+		System.out.println(f.onLine(0.3, 4));
+	}
+	
+	//returns values in pixels
 	public double getValue(int xval){
-		if(functiontype == 1) return Math.sin(constant*xval);
-		if(functiontype == 2) return Math.cos(constant*xval);
-		if(functiontype == 3) return Math.tan(constant*xval);
-		if(functiontype == 5) return constant*xval;
-		if(functiontype == 6) return Math.pow(constant*xval, 2);
-		if(functiontype == 7) return Math.pow(constant*xval, 3);
-		if(functiontype == 8) return Math.pow(constant*xval, 0.5);
-		if(functiontype == 9) return Math.log(constant*xval);
-		return Math.pow(Math.E, constant*xval);
+		if(functiontype == 1) return (int)(Math.sin(constant*xval)*83.3);
+		if(functiontype == 2) return (int)(Math.cos(constant*xval)*83.3);
+		if(functiontype == 3) return (int)(Math.tan(constant*xval)*83.3);
+		if(functiontype == 5) return (int)(constant*xval*83.3);
+		if(functiontype == 6) return (int)(Math.pow(constant*xval, 2)*83.3);
+		if(functiontype == 7) return (int)(Math.pow(constant*xval, 3)*83.3);
+		if(functiontype == 8) return (int)(Math.pow(constant*xval, 0.5)*83.3);
+		if(functiontype == 9) return (int)(Math.log(constant*xval)*83.3);
+		return (int)(Math.pow(Math.E, constant*xval)*83.3);
 	}
 	
 	//detects if coordinates fit function
-	public boolean onLine(int xval, int y){
-		if(functiontype == 1) return y == Math.sin(constant*xval);
-		if(functiontype == 2) return y == Math.cos(constant*xval);
-		if(functiontype == 3) return y == Math.tan(constant*xval);
-		if(functiontype == 5) return y == constant*xval;
-		if(functiontype == 6) return y == Math.pow(constant*xval, 2);
-		if(functiontype == 7) return y == Math.pow(constant*xval, 3);
-		if(functiontype == 8) return y == Math.pow(constant*xval, 0.5);
-		if(functiontype == 9) return y == Math.log(constant*xval);
-		return y == Math.pow(Math.E, constant*xval);
+	public boolean onLine(double xval, int y){
+		if(functiontype == 1) return y == (int)Math.sin(constant*xval);
+		if(functiontype == 2) return y == (int)Math.cos(constant*xval);
+		if(functiontype == 3) return y == (int)Math.tan(constant*xval);
+		if(functiontype == 5) return y == (int)constant*xval;
+		if(functiontype == 6) return y == (int)Math.pow(constant*xval, 2);
+		if(functiontype == 7) return y == (int)Math.pow(constant*xval, 3);
+		if(functiontype == 8) return y == (int)Math.pow(constant*xval, 0.5);
+		if(functiontype == 9) return y == (int)Math.log(constant*xval);
+		return y == (int)Math.pow(Math.E, constant*xval);
 	}
 	
 	public int getFuncType(){ return functiontype; }
@@ -55,7 +60,7 @@ public class Function {
 	
 	public String getFunction(){ 
 		String c = ""+constant;
-		if(constant == 0)
+		if(constant == 1)
 			c = "";
 		if(functiontype == 1) return "y = sin("+c+"x)";
 		if(functiontype == 2) return "y = cos("+c+"x)";
