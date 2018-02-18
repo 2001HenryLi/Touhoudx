d65900270eab9fef0d3f160ad4613c96b20832a7
golfconnor <36574331+golfconnor@users.noreply.github.com>
2018-02-17 21:19:47 -0800

    generic coordinate system

---
+++ src/com/th/Coordinate.java
@@ -0,0 +1,15 @@
+public class Coordinate {
+	
+	private int x;
+	private int y;
+	
+	public Coordinate(int one, int two) {
+		x = one;
+		y = two;
+	}
+
+	public int getX(){ return x; }
+	public int getY(){ return y; }
+	public void setX(int xpos){ x = xpos;}
+	public void setY(int ypos){ y = ypos;}
+}
