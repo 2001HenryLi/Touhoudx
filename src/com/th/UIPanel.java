1c31790d7b4b3939580297fbf5fec7a654a46cfc
Henry Li <2001henryli@gmail.com>
2018-02-18 04:41:01 -0800

    DK DONKEY KONG

--- src/com/th/UIPanel.java
+++ src/com/th/UIPanel.java
@@ -15,7 +15,7 @@ class UIPanel extends JPanel{
         setBackground(Color.WHITE);
         requestFocus();
         setPreferredSize(new Dimension(WIDTH,HEIGHT));
-	guy = p;
+	    guy = p;
     }
 	
     public void paintComponent(Graphics g){
@@ -28,11 +28,15 @@ class UIPanel extends JPanel{
 		Image heart = Toolkit.getDefaultToolkit().getImage("Resources/UISprites/Heart.png");
 		Image card = Toolkit.getDefaultToolkit().getImage("Resources/UISprites/Spellcard.png");
 		g.drawImage(back, 0,0, WIDTH, HEIGHT, this);
-		g.setFont(new Font(Font.SANS_SERIF, Font.TRUETYPE_FONT, 35));
-		g.drawImage(heart, 10, 195, 45, 45,this);
-		g.drawString(""+guy.lives, 50, 218);
-		g.drawImage(card, 10, 255, 45, 45,this);
-		g.drawString(""+guy.bombs, 50, 270);
+		g.setFont(new Font(Font.SANS_SERIF, Font.TRUETYPE_FONT, 80));
+		g.drawImage(heart, 60, 165, 100, 100,this);
+		g.drawString(""+guy.lives, 200, 230);
+		g.drawImage(card, 60, 295, 100, 100,this);
+		g.drawString(""+guy.bombs, 200, 390);
+		g.setColor(Color.WHITE);
+		g.fillRect(70,HEIGHT-230, 400, 100);
+		g.setColor(Color.BLACK);
+        g.drawString(guy.pp.f.getFunction(), 90, HEIGHT-160);
 	}
 
     public void update(){
